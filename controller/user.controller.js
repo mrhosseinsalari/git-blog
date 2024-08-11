3d2be542a86181122095f1b6f0f7d0f59809fd1d
Hossein Salari <hosein.salari81@gmail.com>
2024-08-11 16:30:06 +0330

    add user module to project

---
+++ controller/user.controller.js
@@ -0,0 +1,14 @@
+function create() {
+  //createUser
+}
+function remove() {
+  //deleteUser
+}
+function edit() {
+  //editUser
+}
+function update() {
+  //editUser
+}
+
+// finished develop this file
